86ecca2c627b1422e5240359690472286a6d615e
coloeso <570713991@qq.com>
2022-08-21 22:55:03 +0800

    update

--- C_Single/exercise/封装案例-立方体.cpp
+++ C_Single/exercise/封装案例-立方体.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // 设计立方体类
 // 设计属性,长宽高
-// 设计行为，获取立方体面积和体积
+// 设计行为，获取立方体体积
 // 分别用全局函数和成员函数 判断两个立方体是否相等
 
 struct cube_attr_s
@@ -33,7 +33,7 @@ public:
         return m_attr;
     }
 
-    int set_cube()
+    void set_cube()
     {
         cube = m_attr.m_Length * m_attr.m_Weight * m_attr.m_High;
     }
@@ -43,8 +43,35 @@ public:
         return cube;
     }
 
+    // 成员函数判断两个立方体体积是否相等
+    // 由于成员调用的函数，所以只需要自身和传递进来的另一个立方体的属性作比较，只需要传递一个参数即可
+    bool isSameByClass(Cube &c)
+    {
+        cout << m_attr.m_Length << c.m_attr.m_High << endl;
+        if (m_attr.m_Length == c.m_attr.m_High && m_attr.m_High == c.m_attr.m_High && m_attr.m_Weight == c.m_attr.m_Weight)
+        {
+            return true;
+        }
+        return false;
+    }
+
 };
 
+// 全局函数判断两个立方体体积是否相等
+bool isSame(Cube &c1, Cube &c2)
+{
+    cube_attr_t c1_attr;
+    cube_attr_t c2_attr;
+
+    c1_attr = c1.get_cube_attr();
+    c2_attr = c2.get_cube_attr();
+
+    if (c1_attr.m_Length == c2_attr.m_Length && c1_attr.m_High == c2_attr.m_High && c1_attr.m_Weight == c2_attr.m_Weight)
+    {
+        return true;
+    };
+    return false;
+};
 
 int main()
 {
@@ -60,8 +87,39 @@ int main()
     cube_1.set_cube();
 
 
+    cube_attr_t cube_2_attr;
+
+    cube_2_attr.m_High = 10;
+    cube_2_attr.m_Length = 11;
+    cube_2_attr.m_Weight = 12;
+
+    Cube cube_2;
+
+    cube_2.set_cube_attr(cube_2_attr);
+    cube_2.set_cube();
+
     // cout << "立方体 cube_1 的 长是 %d,宽是 %d, 高是 %d" << "cube_1.get_cube_attr()"  << endl;
-    cout << "立方体 cube_1 的面积是" << cube_1.get_cube()  << endl;
+    cout << "立方体 cube_1 的体积是" << cube_1.get_cube()  << endl;
+    cout << "立方体 cube_2 的体积是" << cube_2.get_cube()  << endl;
+
+    // 利用全局函数判断两个立方体体积是否相等
+    bool ret = isSame(cube_1, cube_2);
+    if (ret)
+    {
+        cout << "立方体 cube_1 和立方体 cube_2 是相等的。" << endl;
+    } else
+    {
+        cout << "立方体 cube_1 和立方体 cube_2 不相等。" << endl;
+    };
+    // 利用成员函数判断两个立方体体积是否相等
+    bool ret2 = cube_1.isSameByClass(cube_2);
+    if(ret2)
+    {
+        cout << "立方体 cube_1 和立方体 cube_2 是相等的。" << endl;
+    } else
+    {
+        cout << "立方体 cube_1 和立方体 cube_2 不相等。" << endl;
+    };
 
     system("pause");
 }
