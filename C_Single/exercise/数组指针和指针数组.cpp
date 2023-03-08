fed0d3249cdd972c1e2656b9bdca720a754d0b93
coloeso <570713991@qq.com>
2023-03-08 23:36:23 +0800

    update

--- C_Single/exercise/数组指针和指针数组.cpp
+++ C_Single/exercise/数组指针和指针数组.cpp
@@ -8,9 +8,32 @@ int array_pointer();
 
 int main()
 {
-    
-    pointer_array();
-    cout << "end..."
+    char temp;
+    bool is_pointer_array = false;
+
+    cout << "指针数组？请输入 Y or N" << endl;
+    cin >> temp;
+
+    if(temp != 'Y' && temp != 'N')
+    {
+        cout << "请输入正确的参数...";
+        return -1;
+    }
+
+    if(temp == 'Y')
+    {
+        is_pointer_array = true;
+    }
+
+    if(is_pointer_array)
+    {
+        pointer_array();
+    } else 
+    {
+        array_pointer();
+    }
+
+    cout << "end...";
 
     return 0;
 }
@@ -34,11 +57,11 @@ int pointer_array()
         cout << *(ptr[i]) << endl;
     }
     
-    char *str = {"apple", "origin", "banana"};
+    const char *str[3] = {"apple", "origin", "banana"};
 
     for(int i=0; i<3; ++i)
     {
-        cout << str[i] << endl;
+        cout << str[i] << endl; /*这里打印的为什么不是地址，而是字符串？？*/
         cout << *(str[i]) << endl;
     }
 
@@ -49,5 +72,22 @@ int pointer_array()
 /*数组指针*/
 int array_pointer()
 {
+    int (*ptr)[4];  /* () 优先级高于 []，ptr 表示一个指向int[4] 的数组的指针，int 修饰数组*/
+
+    int array_1[4] = {11, 22, 33, 44};
+    
+    ptr = &array_1;
+
+    cout << "ptr 的值为 " << ptr << endl;
+
+    for(int i=0; i<4; ++i)
+    {
+        cout << "ptr[" << i << "] 的值为 " << ptr[i] << "";
+        cout << "表示数组第" << i << "个元素的地址" << endl;
+        cout << "*ptr[" << i << "] 的值为 " << *(ptr[i]) << endl;
+        cout << "表示数组第" << i << "个元素的值" << endl;
+    }
+
+    return 0;
 
 }
